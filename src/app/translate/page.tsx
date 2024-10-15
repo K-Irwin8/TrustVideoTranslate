9edfd9fa49fd9ef814b4ae7a7b1450cbe25e3a67
K-irwin8 <Aqlo984@gmail.com>
2024-10-15 18:39:59 +0900

    Added the web app frontend under /translate

---
+++ src/app/translate/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import { VideoTranslationForm } from "@/components/translate/video-translation-form"
+
+export default function Home() {
+  return (<VideoTranslationForm/>);
+}
